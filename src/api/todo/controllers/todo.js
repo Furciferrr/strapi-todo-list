6d4e10b6b10c25b5c4ff1ceec18e0bf16dae0af7
Vadim <vadim.setsko@celadonsoft.com>
2022-03-24 22:42:10 +0300

    customize-controller

--- src/api/todo/controllers/todo.js
+++ src/api/todo/controllers/todo.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  *  todo controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::todo.todo');
+module.exports = createCoreController("api::todo.todo", ({ strapi }) => ({
+  async create(ctx) {
+	const todo = ctx.request.body
+	todo.data.user = ctx.state.user
+ 
+    const entity = await strapi
+      .service("api::todo.todo")
+      .create(todo)
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+	console.log(entity)
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
